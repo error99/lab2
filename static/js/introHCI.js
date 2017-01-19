a5dee3389987c39afde9cab57183eedc94d7ac21
C U D D Y <tcuddy@ucsd.edu>
2017-01-18 23:01:52 -0800

    final commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,27 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+    initializePage();
+});
 
 /*
- * Function that is called when the document is ready.
- */
+* Function that is called when the document is ready.
+*/
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $(".jumbotron h1").text("Javascript is connected");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading
+
+    e.preventDefault();
+    // In an event handler, $(this) refers to
+    // the object that triggered the event
+    $(this).css("background-color", "#7fff00");
 }
